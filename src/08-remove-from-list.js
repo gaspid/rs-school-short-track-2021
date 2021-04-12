6c48e98c08160d0a834f2e92ccb734427d9ef49e
gaspid <gaspid194@gmail.com>
2021-04-12 22:02:53 +0300

    feat:08

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,16 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  let result = l;
+  while (result !== null) {
+    if (result.value === k) {
+      result.value = result.next.value;
+      result.next = result.next.next;
+    }
+    result = result.next;
+  }
+  return l;
 }
 
 module.exports = removeKFromList;
