5f1136653afb045ec9bf183948940636624e1258
gaspid <gaspid194@gmail.com>
2021-04-12 22:04:02 +0300

    feat:11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,19 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const tera = str.split('');
+  let res = '';
+  for (let i = 0; i < tera.length; i++) {
+    const chen = tera[i];
+    let nutt = 1;
+    while (i + 1 < tera.length && chen === tera[i + 1]) {
+      nutt++;
+      i++;
+    }
+    res += nutt === 1 ? chen : `${nutt}${chen}`;
+  }
+  return res;
 }
 
 module.exports = encodeLine;
